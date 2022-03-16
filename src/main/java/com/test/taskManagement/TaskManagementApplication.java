03479a1cf37e86ecbf0206d43f70763bb3220d48
paprika6900 <61785502+paprika6900@users.noreply.github.com>
2022-03-16 12:21:18 +0200

    Initial Project Structure

---
+++ src/main/java/com/test/taskManagement/TaskManagementApplication.java
@@ -0,0 +1,13 @@
+package com.test.taskManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagementApplication.class, args);
+	}
+
+}
